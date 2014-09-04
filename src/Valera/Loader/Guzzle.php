13efbd64dd5cca5beac9f8628765d38949747822
Sergei Morozov <morozov@tut.by>
2014-09-04 22:33:14 +0300

    Use Guzzle parallel adapter

--- src/Valera/Loader/Guzzle.php
+++ src/Valera/Loader/Guzzle.php
@@ -3,18 +3,24 @@
 namespace Valera\Loader;
 
 use GuzzleHttp\ClientInterface;
-use GuzzleHttp\Exception\RequestException;
-use GuzzleHttp\Message\ResponseInterface;
-use Valera\Loader\Result as LoaderResult;
+use GuzzleHttp\Event\CompleteEvent;
+use GuzzleHttp\Event\ErrorEvent;
+use Valera\Queueable;
 use Valera\Resource;
+use Valera\Worker\Converter;
 
-class Guzzle implements LoaderInterface
+class Guzzle implements LoaderInterface, Converter
 {
     /**
      * @var \GuzzleHttp\ClientInterface
      */
     protected $httpClient;
 
+    /**
+     * Constructor
+     *
+     * @param \GuzzleHttp\ClientInterface $httpClient
+     */
     public function __construct(ClientInterface $httpClient)
     {
         $this->setHttpClient($httpClient);
@@ -40,45 +46,44 @@ class Guzzle implements LoaderInterface
         $this->httpClient = $httpClient;
     }
 
-    public function load(Resource $resource, LoaderResult $result)
+    /**
+     * Process tasks
+     *
+     * @param \Traversable|\GuzzleHttp\Message\RequestInterface[] $tasks
+     * @param callable $resolver
+     */
+    public function process(\Traversable $tasks, callable $resolver)
     {
-        try {
-            $response = $this->sendRequest($resource);
-            $this->processResponse($response, $result);
-        } catch (RequestException $e) {
-            $this->processFailure($e, $result);
-        }
+        $this->httpClient->sendAll($tasks, array(
+            'complete' => function (CompleteEvent $event) use ($resolver) {
+                $resolver($event->getRequest(), function (Result $result) use ($event) {
+                    $this->onComplete($event, $result);
+                });
+            },
+            'error' => function (ErrorEvent $event) use ($resolver) {
+                $resolver($event->getRequest(), function (Result $result) use ($event) {
+                    $this->onError($event, $result);
+                });
+            },
+        ));
     }
 
-    protected function sendRequest(Resource $resource)
+    /**
+     * Convert queued item to Guzzle Request
+     *
+     * @param \Valera\Queueable|\Valera\Resource $item
+     * @return \GuzzleHttp\Message\RequestInterface
+     */
+    public function convert(Queueable $item)
     {
-        $request = $this->httpClient->createRequest(
-            $resource->getMethod(),
-            $resource->getUrl(),
+        return $this->httpClient->createRequest(
+            $item->getMethod(),
+            $item->getUrl(),
             array(
-                'headers' => $this->getHeaders($resource),
-                'body' => $resource->getPayload(),
+                'headers' => $this->getHeaders($item),
+                'body' => $item->getPayload(),
             )
         );
-
-        return $this->httpClient->send($request);
-    }
-
-    protected function processResponse(ResponseInterface $response, LoaderResult $result)
-    {
-        $contentType = $response->getHeader('Content-Type');
-        if ($contentType) {
-            $contentType = (string) $contentType;
-        }
-
-        $body = (string) $response->getBody();
-        $result->setContent($body, $contentType);
-    }
-
-    protected function processFailure(RequestException $e, LoaderResult $result)
-    {
-        $message = $e->getMessage();
-        $result->fail($message);
     }
 
     /**
@@ -92,7 +97,37 @@ class Guzzle implements LoaderInterface
     protected function getHeaders(Resource $resource)
     {
         return array_merge(array_filter(array(
-            'referer' => $resource->getReferrer(),
+            'Referer' => $resource->getReferrer(),
         )), $resource->getHeaders());
+    }
+
+    /**
+     * "Complete" event handler
+     *
+     * @param \GuzzleHttp\Event\CompleteEvent $event
+     * @param \Valera\Loader\Result $result
+     */
+    protected function onComplete(CompleteEvent $event, Result $result)
+    {
+        $response = $event->getResponse();
+        $contentType = $response->getHeader('Content-Type');
+        if ($contentType) {
+            $contentType = (string) $contentType;
+        }
+        $body = (string) $response->getBody();
+        $result->setContent($body, $contentType);
+    }
+
+    /**
+     * "Error" event handler
+     *
+     * @param \GuzzleHttp\Event\ErrorEvent $event
+     * @param \Valera\Loader\Result $result
+     */
+    protected function onError(ErrorEvent $event, Result $result)
+    {
+        $exception = $event->getException();
+        $message = $exception->getMessage();
+        $result->fail($message);
     }
 }
